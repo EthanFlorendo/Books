d3b43f6f43ae4168941299d7b07ed19035eab55a
Ethan-Florendo <ethan.florendo1921@gmail.com>
2026-03-19 12:31:58 -0700

    auth

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 export const SUPABASE_URL = 'https://rbtjqlsgfflavrppufbd.supabase.co';
 export const SUPABASE_KEY = 'sb_publishable_heqwqMywY-SgbI2zoFGwLQ_DCp7zDrJ';
 export const USERS = ['Ewan', 'Isaac', 'Ethan', 'Tony'];
-export const ADMIN_EMAIL = 'replace-with-admin-email@example.com';
+export const ADMIN_EMAIL = 'ethan.florendo1921@gmail.com';
